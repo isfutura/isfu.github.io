bff576fd682880a365a143bfcf482d603a5f01b6
Futura <isfutura@protonmail.com>
2021-11-10 17:58:48 -0800

    testing 404, oh yeah, and a functional website

--- scripts/inserts.js
+++ scripts/inserts.js
@@ -1,9 +1,17 @@
 const header = `
-<a href="http://altf4.casa"><img src="/media/bobm.png" style="width: 50px" /></a>
+<a href="/"><img src="/media/bobm.png" style="width: 50px" /></a>
+<h1>${document.title}</h1>
 `;
 
 const footer = `
-<p>keep it real</p>
+<footer>
+<hr>
+<p>thanks for visiting - <a href="https://github.com/twitter/twemoji">twemoji</a></p>
+</footer>
 `;
 
+document.head.innerHTML = `
+<link rel="stylesheet" href="/styles/main.css?v=2">${document.head.innerHTML}`;
 document.body.innerHTML = `${header}${document.body.innerHTML}${footer}`;
+
+twemoji.parse(document.body);
