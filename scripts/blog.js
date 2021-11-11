d5bce1d8cdceca83c5d811b4fe6d732bb5370845
Futura <isfutura@protonmail.com>
2021-11-10 21:02:31 -0800

    blog

---
+++ scripts/blog.js
@@ -0,0 +1,4 @@
+// always put after inserts.js
+
+let titleHeader = document.getElementById('titleHeader');
+titleHeader.innerHTML = `<a href="/blog">back</a>`;
--- scripts/inserts.js
+++ scripts/inserts.js
@@ -1,17 +1,15 @@
 const header = `
 <a href="/"><img src="/media/boom.gif" style="width: 50px" /></a>
-<h1>${document.title}</h1>
+<h1 id="titleHeader">${document.title}</h1>
 `;
 
 const footer = `
 <footer>
 <hr>
-<p>you've reached the bottom - <a href="https://github.com/twitter/twemoji">twemoji</a></p>
+<p>you've reached the bottom</p>
 </footer>
 `;
 
 document.head.innerHTML = `
 <link rel="stylesheet" href="/styles/main.css">${document.head.innerHTML}`;
 document.body.innerHTML = `${header}${document.body.innerHTML}${footer}`;
-
-twemoji.parse(document.body);
