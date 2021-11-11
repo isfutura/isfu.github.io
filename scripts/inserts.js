c3e7c3d47cc7f423d1e693bb8790399b383f7f0c
Futura <isfutura@protonmail.com>
2021-11-10 16:00:59 -0800

    testing inserts 3

--- scripts/inserts.js
+++ scripts/inserts.js
@@ -1,3 +1,9 @@
-fetch('http://altf4.casa/header.html').then(c => {
-  document.body.innerHTML = `${c}${document.body.innerHTML}`;
-});
+const header = `
+<a href="http://altf4.casa"><img src="/media/bobm.png" style="width: 50px" /></a>
+`;
+
+const footer = `
+<p>keep it real</p>
+`;
+
+document.body.innerHTML = `${header}${document.body.innerHTML}${footer}`;
