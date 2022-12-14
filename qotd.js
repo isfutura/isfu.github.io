90eff7593663905cd8f2230cdf30cf8845eabef4
Futura <92005712+isfu@users.noreply.github.com>
2022-12-13 22:11:24 -0800

    clean and simple

--- qotd.js
+++ qotd.js
@@ -1,31 +1,31 @@
-const key = 'c5b5ef2c40b5cc0e19973cf96d38db55';
-const user = 'isfutura';
+const key = "c5b5ef2c40b5cc0e19973cf96d38db55";
+const user = "isfutura";
 
-const endpoint = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${user}&api_key=${key}&format=json`;
+const endpoint =
+  `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${user}&api_key=${key}&format=json`;
 
 (async () => {
   const res = await fetch(endpoint);
   const data = await res.json();
 
-  const quotes = (d => {
+  const quotes = ((d) => {
     let t = [];
-    d.recenttracks.track.forEach(i => {
-      t.push(`${i.name.toLowerCase()} - ${i.artist['#text'].toLowerCase()}`);
+    d.recenttracks.track.forEach((i) => {
+      t.push(`${i.name.toLowerCase()} - ${i.artist["#text"].toLowerCase()}`);
     });
 
     return t;
   })(data);
 
-  const el = document.querySelector('#qotd');
-  const shuffled = quotes.sort(() => Math.random() - 0.5);
+  const el = document.querySelector("#qotd");
 
   let i = 0;
 
-  el.textContent = shuffled[i];
+  el.textContent = quotes[i];
   i++;
 
   setInterval(() => {
-    el.textContent = shuffled[i];
+    el.textContent = quotes[i];
     i++;
   }, 6000);
 })();
