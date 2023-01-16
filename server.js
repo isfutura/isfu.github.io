96930d9cd205c9f217842ef2f4b8d22b452d6c35
Finn <finn@radiohead.PK5001Z>
2023-01-15 16:06:03 -0800

    janked server.js into deno

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-import express from "express";
+// meant to be run with deno
+// originally was node but janked it into deno on 1/15/23 because I didn't want to install node on the linux drive
+
+import express from "npm:express@^4.18.2";
 
 const app = express();
 
